1fbf4aaaac3605a3b2062ebdc08d30a335d46d52
Wojciech Lakomy <wojciech.lakomy@rec-global.com>
2014-09-23 14:04:30 +0200

    Move RPP to separate file

--- dev_apps/privacy-panel/js/guided_tour.js
+++ dev_apps/privacy-panel/js/guided_tour.js
@@ -1,63 +1,30 @@
-'use strict';
+/* global LoadHelper */
 
 (function() {
-
-  /**
-   * Loads a template.
-   */
-  function loadTemplates(callback) {
-    var templates = [
-      document.getElementById('welcome'),
-      document.getElementById('la_explain'),
-      document.getElementById('la_blur'),
-      document.getElementById('la_custom'),
-      document.getElementById('la_exceptions'),
-      document.getElementById('rpp_explain'),
-      document.getElementById('rpp_passphrase'),
-      document.getElementById('rpp_locate'),
-      document.getElementById('rpp_ring'),
-      document.getElementById('rpp_lock')
-    ];
-    window.LazyLoader.load(templates, callback);
-  }
-
-  function showSection(element) {
-    var sections = document.querySelectorAll('section');
-
-    for (var section of sections) {
-      section.style.display = 'none';
-    }
-
-    element.style.display = 'block';
-  }
-
-  function changePage(evt) {
-    var target, id = this.hash.replace('#', '');
-
-    evt.preventDefault();
-
-    if ( ! id) {
-      return;
-    }
-
-    target = document.getElementById(id);
-    showSection(target);
-  }
-
-  function registerEvents() {
-    var links = document.querySelectorAll('.pp-link');
-
-    for (var el of links) {
-      el.addEventListener('click', changePage);
-    }
-  }
+  'use strict';
 
   document.getElementById('menuItem-GT').addEventListener('click', function() {
-    loadTemplates(function() {
-      document.getElementById('root').style.display = 'none';
-      document.getElementById('welcome').style.display = 'block';
-      registerEvents();
-    });
+    window.LazyLoader.load(
+      [
+        document.getElementById('welcome'),
+        document.getElementById('la_explain'),
+        document.getElementById('la_blur'),
+        document.getElementById('la_custom'),
+        document.getElementById('la_exceptions'),
+        document.getElementById('rpp_explain'),
+        document.getElementById('rpp_passphrase'),
+        document.getElementById('rpp_locate'),
+        document.getElementById('rpp_ring'),
+        document.getElementById('rpp_lock')
+      ],
+      function() {
+        document.getElementById('root').style.display = 'none';
+        document.getElementById('welcome').style.display = 'block';
+
+        var sections = document.querySelectorAll('section[data-section="gt"]');
+        LoadHelper.registerEvents(sections);
+      }
+    );
   });
 
 })();
